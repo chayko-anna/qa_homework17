104aa51f4edbf2cdcfd4dd0bcf0882b7caf1a05c
chayko-anna <al.nchko@gmail.com>
2025-01-23 08:53:35 +0300

    fix imports

--- tests/tests_requests.py
+++ tests/tests_requests.py
@@ -1,6 +1,7 @@
 import requests
 from jsonschema import validate
-from schemas import post_user, register_user, update_user, get_user
+import schemas
+from schemas.schemas_users.py import post_user, register_user, update_user, get_user
 
 
 def test_create_user():
@@ -9,15 +10,18 @@ def test_create_user():
     body = response.json()
     validate(body, post_user)
 
+
 def test_get_existing_user_info():
     response = requests.get("https://reqres.in/api/users/2")
     body = response.json()
     assert response.status_code == 200
     validate(body, get_user)
 
+
 def test_get_not_existing_user_info():
     response = requests.get("https://reqres.in/api/users/77")
     assert response.status_code == 404
+
 
 def test_update_user():
     response = requests.put("https://reqres.in/api/users/2", data={"name": "morpheus", "job": "zion resident"})
@@ -25,16 +29,20 @@ def test_update_user():
     body = response.json()
     validate(body, update_user)
 
+
 def test_register_user_successful():
-    response = requests.post("https://reqres.in/api/register", data={"email": "eve.holt@reqres.in", "password": "pistol"})
+    response = requests.post("https://reqres.in/api/register",
+                             data={"email": "eve.holt@reqres.in", "password": "pistol"})
     assert response.status_code == 200
     body = response.json()
     validate(body, register_user)
+
 
 def test_register_user_unsuccessful():
     response = requests.post("https://reqres.in/api/register")
     assert response.status_code == 400
     assert response.text == '{"error":"Missing email or username"}'
+
 
 def test_delete_user_successful():
     response = requests.delete("https://reqres.in/api/users/2")
